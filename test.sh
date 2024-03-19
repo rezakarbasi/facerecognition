c28982061be45f85ad0364b26fbbd69201b70a9b
Reza Karbasi <a.rz.karbasi@gmail.com>
2024-03-19 09:05:43 +0330

    add: a test script added to the project

---
+++ test.sh
@@ -0,0 +1,12 @@
+export LOCAL_PATH="/Users/rezakarbasi/PersonalFiles/Projects/4_OtherProj/didenegar-hiring/AI_Task/AI_Task/files/"
+export VIDEO_NAME="task-video.mp4"
+export LOCAL_VIDEO_PATH="${LOCAL_PATH}${VIDEO_NAME}"
+
+export CONTAINER_PATH="/app/video-folder/"
+export CONTAINER_VIDEO_PATH="${CONTAINER_PATH}${VIDEO_NAME}"
+
+export CONTAINER_ID="facerecognition-my-api-1"
+
+docker cp $LOCAL_VIDEO_PATH $CONTAINER_ID:$CONTAINER_VIDEO_PATH
+curl --location 'http://0.0.0.0:6000/process_video' --form "video_path=${CONTAINER_VIDEO_PATH}"
+docker exec $CONTAINER_ID rm $CONTAINER_VIDEO_PATH
